689470dcc5086023cdbf8d513e8006da14970996
OrionHolcomb <orionholcomb@gmail.com>
2019-03-16 23:56:05 -0400

    Fixed a bug where the letter stayed the same when you won

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -30,14 +30,24 @@ var alphabet = [
 var wins = 0;
 var losses = 0;
 var guessLeft = 9;
-var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
 var guesses = [];
-console.log(randomLetter);
+
+var randomLetter = randomLetter;
 
 document.getElementById("wins").innerHTML = "Coreect Guesses: " + wins;
 document.getElementById("losses").innerHTML = "Times Lost: " + losses;
 document.getElementById("guessleft").innerHTML = "Lives Left: " + guessLeft;
 document.getElementById("guesses").innerHTML = "Guesses So far: " + guesses;
+
+function randomL() {
+  randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+  console.log(randomLetter);
+}
+
+randomL();
+
+// Poking fun at my buddy
+console.log("Robert sux!");
 
 document.onkeypress = function userKey(event) {
   var userGuess = event.key;
@@ -46,6 +56,7 @@ document.onkeypress = function userKey(event) {
     wins++;
     guessLeft = 9;
     guesses = [];
+    randomL();
   }
 
   if (userGuess !== randomLetter) {
@@ -62,8 +73,9 @@ document.onkeypress = function userKey(event) {
   console.log("Wins: " + wins);
   console.log("Losses: " + losses);
   console.log("Guesses Left: " + guessLeft);
+  
 
-  document.getElementById("wins").innerHTML = "Coreect Guesses: " + wins;
+  document.getElementById("wins").innerHTML = "Correct Guesses: " + wins;
   document.getElementById("losses").innerHTML = "Times Lost: " + losses;
   document.getElementById("guessleft").innerHTML = "Lives Left: " + guessLeft;
   document.getElementById("guesses").innerHTML = "Guesses So far: " + guesses;
